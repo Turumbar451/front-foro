1d20064a8097419e306461b44cdf72bbc20d0d95
Emir Tirso <turumbar451@hotmail.com>
2025-12-10 10:26:00 -0600

    permitir domino de railway en vite 11

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,33 +6,39 @@ import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 import node from '@astrojs/node';
+const ALLOWED_HOSTS = [
+  'localhost',
+  '127.0.0.1',
+  'front-foro-production.up.railway.app',
+  'front-foro.onrender.com',
+  '*', // El comodín universal como último recurso
+];
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', //indicamos que usaremos un server para desplegar
+  output: 'server',
   integrations: [svelte(), tailwind()],
 
   vite: {
-    server: {
-      allowedHosts: [
-        'front-foro-production.up.railway.app',
-        'localhost',
-        '127.0.0.1',
-        '*'
-      ],
-    },
     preview: {
-      allowedHosts: [
-        'front-foro-production.up.railway.app',
-        'localhost',
-        '127.0.0.1',
-        '*'
-      ],
       host: true,
-      port: 4321
+      port: 4321,
+      allowedHosts: ALLOWED_HOSTS,
+    },
+
+
+    server: {
+      host: true,
+      port: 4321,
+      allowedHosts: ALLOWED_HOSTS,
+
+      hmr: {
+        host: 'localhost',
+        protocol: 'ws',
+        clientPort: 4321
+      }
     }
   },
-
 
   adapter: node({
     mode: 'standalone'
